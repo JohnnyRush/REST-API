39220e5388449f1e1bd59751083921add4ba036e
JohnnyRush <pospiechjan@gmail.com>
2019-11-04 23:43:04 +0100

    22.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -20,20 +20,8 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.stream()
-                .filter(t -> t.getId() != null)
-                .filter(t -> t.getName() != null)
-                .filter(t -> t.getName().contains("Kodilla"))
-                .forEach(t -> {
-                    System.out.println(t.getId() + " " + t.getName());
-                    System.out.println("This board contains lists: ");
-                    t.getLists().forEach(trelloList ->
-                            System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-                });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
